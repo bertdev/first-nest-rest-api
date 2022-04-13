7eee53c26284ba87e1c4d86aacc855e9d8137ba0
bertdev <herbertmatos10@hotmail.com>
2022-04-12 21:11:30 -0300

    add data validation to validate request data for sign  up using dto and validation pipes

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,9 +1,9 @@
-import { Injectable } from "@nestjs/common";
-import { PrismaService } from "src/prisma/prisma.service";
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable({})
 export class AuthService {
-  constructor(private prisma: PrismaService) { }
+  constructor(private prisma: PrismaService) {}
 
   signup() {
     return { message: 'I am signed up' };
@@ -12,5 +12,4 @@ export class AuthService {
   signin() {
     return { message: 'I am signed in' };
   }
-
 }
