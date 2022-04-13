6b55110ab27cecc1caa2d828f785be24e0c46c3c
bertdev <herbertmatos10@hotmail.com>
2022-04-13 16:46:34 -0300

    add the error thrown when a user tries to create a register using an email that has already been taken

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { AuthDto } from './dto';
 import * as argon from 'argon2';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
 
 @Injectable({})
 export class AuthService {
@@ -9,18 +10,27 @@ export class AuthService {
 
   async signup(dto: AuthDto) {
     const hash = await argon.hash(dto.password);
-    const user = await this.prisma.user.create({
-      data: {
-        email: dto.email,
-        password: hash
-      },
-      select: {
-        id: true,
-        email: true,
-        createdAt: true
+    try {
+      const user = await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          password: hash
+        },
+        select: {
+          id: true,
+          email: true,
+          createdAt: true
+        }
+      });
+      return user;
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        if (error.code === 'P2002') {
+          throw new ForbiddenException('Credentials taken');
+        }
       }
-    });
-    return user;
+      throw error;
+    }
   }
 
   signin() {
