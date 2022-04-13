feba1fc78d8934991f1bb66f7e8b530636426f59
bertdev <herbertmatos10@hotmail.com>
2022-04-12 21:22:56 -0300

    add the logic to create a user and encrypt his password

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,12 +1,26 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthDto } from './dto';
+import * as argon from 'argon2';
 
 @Injectable({})
 export class AuthService {
-  constructor(private prisma: PrismaService) {}
+  constructor(private prisma: PrismaService) { }
 
-  signup() {
-    return { message: 'I am signed up' };
+  async signup(dto: AuthDto) {
+    const hash = await argon.hash(dto.password);
+    const user = await this.prisma.user.create({
+      data: {
+        email: dto.email,
+        password: hash
+      },
+      select: {
+        id: true,
+        email: true,
+        createdAt: true
+      }
+    });
+    return user;
   }
 
   signin() {
